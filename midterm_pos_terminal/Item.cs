35301eef01f2a515f1b14f243b080c43d6c82aec
Will Schoenle (IT - IT_CORE) <William.Schoenle@dominos.com>
2021-05-20 21:07:56 -0400

    pushing parent interface

---
+++ midterm_pos_terminal/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace midterm_pos_terminal
+{
+    public interface Item
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+
+    }
+}
