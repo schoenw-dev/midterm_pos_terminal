81e645ebb3ae6057c20fb0297a38aa1538d97ef4
AnfernyReloded <78870003+AnfernyReloded@users.noreply.github.com>
2021-05-24 21:53:13 -0400

    Added text file reader and writer, specifics still needed

---
+++ midterm_pos_terminal/Bowls.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace midterm_pos_terminal
+{
+    public class Bowls : ItemProperties
+    {
+        public string _flavors { get; set; }
+
+        public Bowls()
+        {
+
+        }
+        public Bowls (string flavor)
+        {
+            _flavors = flavor;
+        }
+
+        //generate list of flavors: vanilla, chocolate, stawberry, superman, butter pecan
+        //generate list of toppings: m&m's, reases pieces, sprinkles, oreos
+        //generate list of sizes: small, medium, large
+
+        public static List<Bowls> GetFlavors()
+        {
+            List<Bowls> bowls = new List<Bowls>
+            {
+                new Bowls ("vanilla"),
+                new Bowls ("chocolate"),
+                new Bowls ("strawberry"),
+                new Bowls ("superman"),
+                new Bowls ("butter pecan")
+
+
+            };
+
+            return bowls;
+
+        }
+    }
+}
---
+++ midterm_pos_terminal/FileIO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace midterm_pos_terminal
+{
+    public class FileIO
+
+    {  //missing specific referneces
+        
+        public static void WriteMenu (List<ItemProperties> itemProperties)
+        {
+            StreamWriter writer = new StreamWriter("", true);
+            foreach (ItemProperties itemProperties1 in itemProperties)
+            {
+                writer.WriteLine();
+            }
+        }
+
+        public static List<ItemProperties> GetMenu(string path)
+        {
+            //define file path later
+            List<ItemProperties> itemProperties = new List<ItemProperties>();
+            StreamReader reader = new StreamReader("");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                //nail down lists and properites of our items
+            }
+            reader.Close();
+            return itemProperties;
+        }
+    }
+}
