097e577d9087638d6dff4733b21bd937e1065589
TxnUkeGrl <txnukegrl@gmail.com>
2021-05-24 21:26:25 -0400

    mostly done softserve item, added other properties to items

--- midterm_pos_terminal/ConesSoftServe.cs
+++ midterm_pos_terminal/ConesSoftServe.cs
@@ -6,12 +6,40 @@ namespace midterm_pos_terminal
 {
     public class ConesSoftServe : ItemProperties
     {
-        public ConesSoftServe(string name, double price, string size, string topping)
+        public static bool Dipped { get; set; }
+
+        public ConesSoftServe(string name, double price, string size, string description, double subtotal, bool dipped)
         {
             Name = name;
             Price = price;
-            Size = size;
-            Topping = topping;
+            CategorySize = size;
+            Subtotal = subtotal;
+            Dipped = dipped;
+        }
+
+        public static List<string> ChooseCreamFlavor()
+        {
+            List<string> flavors = new List<string>
+            {
+                "vanilla", "chocolate", "strawberry", "mint chocolate chip"
+            };
+
+            return flavors;
+        }
+
+        public void ChocolateDipped()
+        {
+            Console.WriteLine("chocolate dipped?");
+            if (Console.ReadLine().ToUpper() == "Y")
+            {
+                Dipped = true;
+                Subtotal = Subtotal + 0.50;
+            }
+            else
+            {
+                Dipped = false;
+                Price = 1.50;
+            }
         }
     }
 }
