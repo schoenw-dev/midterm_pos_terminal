48580b469657ebb2be84f7b5e86e073c3a4373af
TxnUkeGrl <txnukegrl@gmail.com>
2021-05-27 20:46:44 -0400

    method for creating softserve

--- midterm_pos_terminal/ConesSoftServe.cs
+++ midterm_pos_terminal/ConesSoftServe.cs
@@ -10,19 +10,94 @@ namespace midterm_pos_terminal
         //public static string ConeType { get; set; }
         public string SoftServeFlavor { get; set; }
 
-        public ConesSoftServe(string name, double price, string description, double subtotal, string size, string conetype, bool dipped, string softServeFlavor)
+        public ConesSoftServe(string name, double price)
         {
             Name = name;
             Price = price;
-            Subtotal = subtotal;
-            CategorySize = size; //feature #1
-            //ConeType = conetype; //feature #2
-            Dipped = dipped; // feature #3
-            SoftServeFlavor = softServeFlavor; //feature #4 - other 'list' of flavors to reference?
+        }
+        
+        public class SoftServeSize : ItemProperties
+        {
+            public SoftServeSize(string _name, double _price)
+            {
+                Name = _name;
+                Price = _price;
+            }
+        }
 
-            Name = $"{_hotMain.Name} with {_side.Name}";
-            Subtotal = Subtotal + sizes.Price + cones.Price + dipped.Price + flavors.Price;
-            ConeType.ReferenceEquals(Console.ReadLine(), ConeType.sugar);
+        public class SoftServeCone : ItemProperties
+        {
+            public SoftServeCone(string _name, double _price)
+            {
+                Name = _name;
+                Price = _price;
+            }
+        }
+
+        public class SoftServeDip : ItemProperties
+        {
+            public SoftServeDip(string _name, double _price)
+            {
+                Name = _name;
+                Price = _price;
+            }
+        }
+
+        //public class SoftServeFlavors : ItemProperties
+        //{
+        //    public SoftServeFlavors(string _name, double _price)
+        //    {
+        //        Name = _name;
+        //        Price = _price;
+        //    }
+        //}
+
+        //public ConesSoftServe(string name, double price, string description, double subtotal, string size, string conetype, bool dipped, string softServeFlavor)
+        //{
+        //    Name = name;
+        //    Price = price;
+        //    Subtotal = subtotal;
+        //    CategorySize = size; //feature #1
+        //    //ConeType = conetype; //feature #2
+        //    Dipped = dipped; // feature #3
+        //    SoftServeFlavor = softServeFlavor; //feature #4 - other 'list' of flavors to reference?
+        //}
+
+        public static (List<SoftServeSize>, List<SoftServeCone>, List<SoftServeDip>, List<string>) SoftServeElements()
+        {
+            List<SoftServeSize> softServeSize = new List<SoftServeSize>
+            {
+                new SoftServeSize("kids", 0.25),
+                new SoftServeSize("small", 0.75),
+                new SoftServeSize("medium", 1.50),
+                new SoftServeSize("large", 4.00)
+            };
+
+            List<SoftServeCone> coneType = new List<SoftServeCone>
+            {
+                new SoftServeCone("no cone", 0.00),
+                new SoftServeCone("sugar", 1.00),
+                new SoftServeCone("waffle", 2.00),
+                new SoftServeCone("cake", 0.50)
+            };
+
+            List<SoftServeDip> dip = new List<SoftServeDip>
+            {
+                new SoftServeDip("not dipped", 0.00),
+                new SoftServeDip("chocolate", 0.50),
+                new SoftServeDip("chocolate with sprinkles", 0.75)
+            };
+
+            List<string> flavor = new List<string>
+            {
+                new string("vanilla"),
+                new string("chocolate"),
+                new string("strawberry"),
+                new string("mint chocolate chip"),
+                new string("bubble gum")
+            };
+
+            return (softServeSize, coneType, dip, flavor);
         }
 
         //public static List<string> ChooseSize() //feature #1; method to actually choose size, not just list out
@@ -37,13 +112,13 @@ namespace midterm_pos_terminal
 
         //REFERENCE FOR STRUCT: https://stackoverflow.com/questions/14744123/can-you-use-enum-for-double-variables
 
-        public struct ChooseSize
-        {
-            public const double kids = .25;
-            public const double small = .75;
-            public const double medium = 1.50;
-            public const double large = 4;
-        }
+        //public struct ChooseSize
+        //{
+        //    public const double kids = .25;
+        //    public const double small = .75;
+        //    public const double medium = 1.50;
+        //    public const double large = 4;
+        //}
 
         //public static List<string> ChooseConeType() //feature #2; method to actually choose cone type, not just list out; add prices
         //{
@@ -52,37 +127,41 @@ namespace midterm_pos_terminal
         //        "sugar", "waffle", "cake", "no cone"
         //    };
 
-        public struct ConeType
-        {
-            public const double sugar = 1;
-            public const double waffle = 2;
-            public const double cake = .50;
-            public const double none = 0;
-        }
+        //public struct ConeType
+        //{
+        //    public const double sugar = 1;
+        //    public const double waffle = 2;
+        //    public const double cake = .50;
+        //    public const double none = 0;
+        //}
 
-        public void ChocolateDipped() // feature #3
-        {
-            Console.WriteLine("chocolate dipped?");
-            if (Console.ReadLine().ToUpper() == "Y")
-            {
-                Dipped = true;
-                subtotal = subtotal + 0.50;
-            }
-            else
-            {
-                dipped = false;
-            }
-        }
+        //public void ChocolateDipped() // feature #3
+        //{
+        //    Console.WriteLine("chocolate dipped?");
+        //    if (Console.ReadLine().ToUpper() == "Y")
+        //    {
+        //        Dipped = true;
+        //        subtotal = subtotal + 0.50;
+        //    }
+        //    else
+        //    {
+        //        dipped = false;
+        //    }
+        //}
 
-        public static List<string> ChooseCreamFlavor() //feature #4; method to actually choose flavor, not just list out
-        {
-            List<string> flavors = new List<string>
-            {
-                "vanilla", "chocolate", "strawberry", "mint chocolate chip"
-            };
+        //public static List<string> ChooseCreamFlavor() //feature #4; method to actually choose flavor, not just list out
+        //{
+        //    List<string> flavors = new List<string>
+        //    {
+        //        "vanilla", "chocolate", "strawberry", "mint chocolate chip"
+        //    };
 
-            Subtotal = Subtotal + 2;
-            return flavors;
-        }
+        //    Subtotal = Subtotal + 2;
+        //    return flavors;
+        //}
+
+        //Name = $"{_hotMain.Name} with {_side.Name}";
+        //Subtotal = Subtotal + sizes.Price + cones.Price + dipped.Price + flavors.Price;
+        //ConeType.ReferenceEquals(Console.ReadLine(), ConeType.sugar);
     }
 }
